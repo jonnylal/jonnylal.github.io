1456b5a293d4564c3bec0fdf31c77ce1985142b1
unknown <jlal@usc.edu>
2021-08-17 15:48:44 -0700

    What I have so far?

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jesse Hicks",
+        company: "Zoozle",
+    },
+    {
+        quote:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/94.jpg",
+        name: "Ruben Alvarez",
+        company: "Dooble.io",
+    },
+];
+
+export const skills = [
+    "C/C++",
+    "Python",
+    "Java",
+    "JavaScript",
+    "Node",
+    "React",
+    "",
+    "test2",
+];
